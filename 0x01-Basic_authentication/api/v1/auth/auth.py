9fa1f5ee9d6eaf102e4fc73366b784b321e9f129
iradukundapaci <paifique@gmail.com>
2024-04-16 18:12:32 +0200

    adding task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Model for basic authentication
+"""
+from flask import Request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class for basic authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Validate if path requiers authentication
+
+        args:
+            path: path to validate
+            excluded_paths: paths that don't require authentication
+
+        return: True if path requires authentication else False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieve request header
+
+        args:
+            request: flask request object
+
+        return: request authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """
+        Retrieve request user
+
+        args:
+            request: flask request object
+
+        return: user
+        """
+        return None
