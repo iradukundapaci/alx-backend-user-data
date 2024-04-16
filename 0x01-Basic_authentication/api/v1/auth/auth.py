afceb73fb97d99cb07ffd819ed40e4006835db96
iradukundapaci <paifique@gmail.com>
2024-04-16 18:43:53 +0200

    handling require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,14 @@ class Auth:
 
         return: True if path requires authentication else False
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for url in excluded_paths:
+            if path.rstrip("/") == url.rstrip("/"):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
